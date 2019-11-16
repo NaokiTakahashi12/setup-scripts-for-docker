0cb8ebfc14e8be12e59348c8131a29e4183bdaca
NaokiTakahashi <>
2019-11-16 09:18:56 +0900

    Added setup script for nvidia container

---
+++ setup-nvidia-container.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+REQUIREDVERSION="19.03.4"
+DOCKERVERSION=$(docker version --format '{{.Client.Version}}')
+
+echo "Docker Client version is $DOCKERVERSION"
+echo "Required version is $REQUIREDVERSION"
+
+UNDER=$(echo "$REQUIREDVERSION" | awk -F '[.]' '{print $1$2$3}')
+DOCKERVERSION=$(echo "$DOCKERVERSION" | awk -F '[.]' '{print $1$2$3}')
+
+if [ $DOCKERVERSION -ge $UNDER ]; then
+        echo "Start setup for nvidia-container"
+        distribution=$(. /etc/os-release; echo $ID$VERSION_ID) && \
+        curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add - && \
+        curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list && \
+        sudo apt update && sudo apt install -y nvidia-container-runtime && \
+        echo "Finish setup"
+
+else
+        echo "Please upgrade the Docker version to $REQUIREDVERSION or higher."
+
+fi
